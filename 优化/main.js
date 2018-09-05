5f3b33c8b76d0d71394e3b92b4401f61dae0859b
jerryHZLiang <798895110@qq.com>
2018-09-05 21:31:48 +0800

    first version

---
+++ 优化/main.js
@@ -0,0 +1,49 @@
+
+let n
+初始化()
+setInterval(() => {
+
+    makeLeave(getImage(n))
+
+     .one('transitionend', (e) => {
+         makeEnter($(e.currentTarget))
+     })
+    makeCurrent(getImage(n+1))
+    n += 1
+},3000)
+
+
+
+
+
+function x(n){
+    if(n>3){
+        n = n%3
+        if(n===0){
+            n=3
+        }
+    }
+    return n
+}
+
+function getImage(n){
+   return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function 初始化(){
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return $node.removeClass('leave enter').addClass('current')
+}
+
+function makeLeave($node){
+    return $node.removeClass('current enter').addClass('leave')
+}
+
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
